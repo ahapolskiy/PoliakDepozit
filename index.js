d95027cae895bce408fcc5bac3d32a3f4df33ba4
ahapolskiy <104796003+ahapolskiy@users.noreply.github.com>
2022-05-03 01:52:43 +0500

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,20 @@
+<style type="text/css">
+    input[type="checkbox"] + span {
+        position: relative;
+        top: -2px;
+        left: 3px;
+    }
+    input[type="checkbox"]:checked + span {
+        color: #039;
+        text-decoration: underline;
+    }
+</style>
+<p>Какой напиток Вы предпочитаете пить по утрам?</p>
+<div style="float:left; margin-right: 50px;">
+    <label><input type="checkbox" checked name="a1"><span>Кофе</span></label><br>
+    <label><input type="checkbox" name="a2"><span>Чай</span></label><br>
+    <label><input type="checkbox" name="a3"><span>Какао</span></label>
+</div>
+<label><input type="checkbox" name="a4"><span>Сок</span></label><br>
+<label><input type="checkbox" name="a5"><span>Вода</span></label><br>
+<label><input type="checkbox" name="a6"><span>Рассол</span></label>
